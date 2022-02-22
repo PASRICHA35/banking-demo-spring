bd1735328a07cac90d201ac99e32cc67e0ff0efb
aryanpasricha <aryan.pasricha@credable.in>
2022-02-22 10:01:09 +0530

    added spring bean to peoject.

--- src/main/java/org/example/app/Application.java
+++ src/main/java/org/example/app/Application.java
@@ -1,14 +1,25 @@
+//#cleanup command
+//        docker rm -f $(docker ps -aq)
+
+//#run mysql
+//        docker run -d --rm -p 3306:3306 thecodecamp/customers-db:latest
+
+//for status
+//docker ps -a
+
 package org.example.app;
 
+import org.example.AppConfig;
 import org.example.model.Customer;
 import org.example.repository.CustomerRepositoryDB;
 import org.example.repository.CustomerRepositoryStub;
 import org.example.service.CustomerService;
 import org.example.service.DefaultCustomerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 
-//Ioc Container(Inversion of Control)
 public class Application {
 
     public static void main(String[] args) {
@@ -23,7 +34,13 @@ public class Application {
 //        DefaultCustomerService service = new DefaultCustomerService(dbRepository);
 
         //also can call like this
-        CustomerService service = new DefaultCustomerService(newCustomerRepositoryStub());
+//        CustomerService service = new DefaultCustomerService(newCustomerRepositoryDB());
+
+        //now by AppConfig
+        //Ioc Container(Inversion of Control)
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        CustomerService service=context.getBean("customerService",CustomerService.class);
+
 
 //    List<Customer> customers = service.getAllCustomersWithStars();
         List<Customer> customers = service.getAllCustomers();
@@ -33,11 +50,11 @@ public class Application {
     }
 
     //aslo can call like this bcoz its IOC.
-    public static CustomerRepositoryDB newCustomerRepositoryDB() {
-        return new CustomerRepositoryDB();
-    }
-
-    public static CustomerRepositoryStub newCustomerRepositoryStub() {
-        return new CustomerRepositoryStub();
-    }
+//    public static CustomerRepositoryDB newCustomerRepositoryDB() {
+//        return new CustomerRepositoryDB();
+//    }
+//
+//    public static CustomerRepositoryStub newCustomerRepositoryStub() {
+//        return new CustomerRepositoryStub();
+//    }
 }
