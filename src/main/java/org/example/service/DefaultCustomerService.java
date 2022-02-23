a7a7ea67f32fa77da09d8737fe367958998137d7
aryanpasricha <aryan.pasricha@credable.in>
2022-02-23 10:05:08 +0530

    xml is working fine, now no need of AppConfig.java

--- src/main/java/org/example/service/DefaultCustomerService.java
+++ src/main/java/org/example/service/DefaultCustomerService.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Service("customerService")
+//@Service("customerService")
 public class DefaultCustomerService implements CustomerService {
 
     //or write this for what repo we want to run.
